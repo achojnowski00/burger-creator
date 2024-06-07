7b53001db2cb5d4124f6be838b7d883afe20f91b
achojnowski00 <achojnowski00@users.noreply.github.com>
2024-06-07 15:52:10 +0000

    deploy: 0f3a1db7775dbc6be1c37348fc771b8e439e9908

---
+++ assets/AppHeader-e3ec852c.js
@@ -0,0 +1,1 @@
+import{j as r,c as p}from"./index-b86ae710.js";const c=({className:e,text:s,Component:a="h1"})=>r.jsx(a,{className:p("app-header",e),children:s});export{c as A};
--- assets/index-ea91ad30.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-30602f84.js";import{A as e}from"./AppHeader-ba6ed043.js";const t=()=>r.jsx("div",{children:r.jsx(e,{text:"Favuorite Burgers"})}),a=t;export{a as default};

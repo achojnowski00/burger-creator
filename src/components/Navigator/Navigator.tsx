c46591ce908852ad301b671213eff1a84b7a9097
Aleksander Chojnowski <achojnowski00@gmail.com>
2024-06-07 12:22:02 +0200

    fix wrong path

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { NavigatorRoutes } from './NavigatorRoutes';
 
-import './Navigator.scss';
+import './navigator.scss';
 
 type IProps = IComponent;
 
